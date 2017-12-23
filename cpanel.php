46bec57f790968d7ad0729044492a0297f1d3958
Mit <vasanimit9@gmail.com>
2017-12-23 12:04:09 +0530

    cpanel with add user form

--- cpanel.php
+++ cpanel.php
@@ -17,18 +17,31 @@
 		<br>
 
 		<div class="row">
-			<div class="col-md-3" style="font-size: 2em;">
-				<i class="fa fa-user-plus"></i> Add User<hr>
-			</div>
-			<div id="vr"></div>
-			<div class="col-md-9" id="win">
-				
+			<div class="col-md-9"></div>
+			<div class="col-md-3" style="text-align: center;">
+				<h3>Add User</h3>
+				<input type="email" name="email" placeholder="Email" class="form-control" required>
+				<input type="password" name="password" placeholder="Password" class="form-control" required="">
+				<select name="user_type" class="form-control" onfocus="hid('hidop')">
+					<option id="hidop">--type--</option>
+					<option value="5">Driver</option>
+					<option value="4">School Principal</option>
+					<?php	if($_SESSION["type"]<3) { ?>
+					<option value="3">Executive</option>
+					<?php }
+							if($_SESSION["type"]==1) { ?>
+					<option value="2">Admin</option>
+					<option value="1">SuperAdmin</option>
+					<?php } ?>
+				</select><br>
+				<button type="submit" class="btn btn-success form-control">Add</button>
 			</div>
 		</div>
 
 	</div>
 
 	<script type="text/javascript">
-			$("#vr").css("height",$("#win").height);
-			$("#win").css("height",window.innerHeight);
+		function hid(x) {
+			$("#"+x).css("display","none");
+		}
 	</script>
