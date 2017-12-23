abc689c923b6259b21e97cf89f0e46405d0c1d3c
Mit <vasanimit9@gmail.com>
2017-12-23 12:13:59 +0530

    cpanel with add user form

--- cpanel.php
+++ cpanel.php
@@ -20,21 +20,23 @@
 			<div class="col-md-9"></div>
 			<div class="col-md-3" style="text-align: center;">
 				<h3>Add User</h3>
-				<input type="email" name="email" placeholder="Email" class="form-control" required>
-				<input type="password" name="password" placeholder="Password" class="form-control" required="">
-				<select name="user_type" class="form-control" onfocus="hid('hidop')">
-					<option id="hidop">--type--</option>
-					<option value="5">Driver</option>
-					<option value="4">School Principal</option>
-					<?php	if($_SESSION["type"]<3) { ?>
-					<option value="3">Executive</option>
-					<?php }
-							if($_SESSION["type"]==1) { ?>
-					<option value="2">Admin</option>
-					<option value="1">SuperAdmin</option>
-					<?php } ?>
-				</select><br>
-				<button type="submit" class="btn btn-success form-control">Add</button>
+				<form action="adduser.php" method="post">
+					<input type="email" name="email" placeholder="Email" class="form-control" required>
+					<input type="password" name="password" placeholder="Password" class="form-control" required="">
+					<select name="user_type" class="form-control" onfocus="hid('hidop')">
+						<option id="hidop">--type--</option>
+						<option value="5">Driver</option>
+						<option value="4">School Principal</option>
+						<?php	if($_SESSION["type"]<3) { ?>
+						<option value="3">Executive</option>
+						<?php }
+								if($_SESSION["type"]==1) { ?>
+						<option value="2">Admin</option>
+						<option value="1">SuperAdmin</option>
+						<?php } ?>
+					</select><br>
+					<button type="submit" class="btn btn-success form-control">Add</button>
+				</form>
 			</div>
 		</div>
 
