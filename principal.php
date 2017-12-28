d03a5dbd2d29815c2c003092e547a600bc33cd70
Rohit Chaku <rohit98chaku@gmail.com>
2017-12-28 22:51:05 +0530

    Principal Frontend

--- principal.php
+++ principal.php
@@ -56,49 +56,65 @@
     <div class="row justify-content-center">
       <h5><i>Here is today's menu for your school, <?php echo "$school_name"; ?>!</i></h5>
     </div>
-    <table align="center" class="table table-striped">
-      <tr>
-        <td align="center"><strong>Item</strong></td>
-        <td align="center"><strong>Small</strong></td>
-        <td align="center"><strong>Medium</strong></td>
-        <td align="center"><strong>Large</strong></td>
-        <td align="center"><strong>Num/Kg</strong></td>
-        <td align="center"><strong>Total(in M)</strong></td>
-      </tr>
-      <tr>
-        <td align="center"><strong>Rice</strong></td>
-        <td align="center"><?php echo "$ril"; ?></td>
-        <td align="center"><?php echo "$rim"; ?></td>
-        <td align="center"><?php echo "$ris"; ?></td>
-        <td align="center"><?php echo "$rkg"; ?></td>
-        <td align="center"><?php $totri = $ril*1.5 + $rim + $ris*0.33; echo "$totri"; ?></td>
-      </tr>
-      <tr>
-        <td align="center"><strong>Dal</strong></td>
-        <td align="center"><?php echo "$das"; ?></td>
-        <td align="center"><?php echo "$dam"; ?></td>
-        <td align="center"><?php echo "$dal"; ?></td>
-        <td align="center"><?php echo "$dkg"; ?></td>
-        <td align="center"><?php $totda = $dal*1.5 + $dam + $das*0.33; echo "$totda"; ?></td>
-      </tr>
-      <tr>
-        <td align="center"><strong>Roti</strong></td>
-        <td align="center">0</td>
-        <td align="center"><?php echo "$rom"; ?></td>
-        <td align="center"><?php echo "$rol"; ?></td>
-        <td align="center"><?php echo "$rnum"; ?></td>
-        <td align="center"><?php $totro = $rnum; echo "$totro"; ?></td>
-      </tr>
-      <tr>
-        <td align="center"><strong>SideDish</strong></td>
-        <td align="center"><?php echo "$sideItem"; ?></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td align="center"><?php echo "$sideItem"; ?></td>
-      </tr>
-    </table>
+    <div class="row justify-content-center">
+      <div class="col-sm-9 col-md-9">
+        <table align="center" class="table table-striped">
+          <tr>
+            <td align="center"><strong>Item</strong></td>
+            <td align="center"><strong>Small</strong></td>
+            <td align="center"><strong>Medium</strong></td>
+            <td align="center"><strong>Large</strong></td>
+            <td align="center"><strong>Num/Kg</strong></td>
+            <td align="center"><strong>Total(in M)</strong></td>
+          </tr>
+          <tr>
+            <td align="center"><strong>Rice</strong></td>
+            <td align="center"><?php echo "$ril"; ?></td>
+            <td align="center"><?php echo "$rim"; ?></td>
+            <td align="center"><?php echo "$ris"; ?></td>
+            <td align="center"><?php echo "$rkg"; ?></td>
+            <td align="center"><?php $totri = $ril*1.5 + $rim + $ris*0.33; echo "$totri"; ?></td>
+          </tr>
+          <tr>
+            <td align="center"><strong>Dal</strong></td>
+            <td align="center"><?php echo "$das"; ?></td>
+            <td align="center"><?php echo "$dam"; ?></td>
+            <td align="center"><?php echo "$dal"; ?></td>
+            <td align="center"><?php echo "$dkg"; ?></td>
+            <td align="center"><?php $totda = $dal*1.5 + $dam + $das*0.33; echo "$totda"; ?></td>
+          </tr>
+          <tr>
+            <td align="center"><strong>Roti</strong></td>
+            <td align="center">0</td>
+            <td align="center"><?php echo "$rom"; ?></td>
+            <td align="center"><?php echo "$rol"; ?></td>
+            <td align="center"><?php echo "$rnum"; ?></td>
+            <td align="center"><?php $totro = $rnum; echo "$totro"; ?></td>
+          </tr>
+          <tr>
+            <td align="center"><strong>SideDish</strong></td>
+            <td align="center"><?php echo "$sideItem"; ?></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td align="center"><?php echo "$sideItem"; ?></td>
+          </tr>
+        </table>
+      </div>
+    </div>
+
   <?php } ?>
+
+  <div class="row justify-content-center">
+    <form action="principalCheck.php" method="post">
+        <input type="submit" class="btn btn-outline-success" name="submit" value="Time Check!">
+    </form>
+  </div>
+  <div class="row justify-content-center">
+    <small class="text-muted">Click here if your food has been delivered!</small>
+  </div>
+  <br>
+
   <div class="container">
     <div class="row justify-content-center">
       <h3>Your Foodback Please!</h3>
@@ -106,8 +122,254 @@
     <div class="row justify-content-center">
       <small class="text-muted">Help us make our food better.</small>
     </div>
+    <br>
     <div class="row justify-content-center">
-      
+      <form action="feedback.php" method="post">
+        <div class="form-check">
+          <label class="form-check-label">
+            <input class="form-check-input" type="radio" name="quality" value="good" checked>
+            The food was good.
+          </label>
+        </div>
+        <div class="form-check">
+          <label class="form-check-label">
+            <input class="form-check-input" type="radio" name="quality" value="ngood">
+            The food was not good.
+          </label>
+        </div>
+        <div class="form-check">
+          <label class="form-check-label">
+            <input class="form-check-input" type="radio" name="quality" value="nwarm">
+            The food was not warm.
+          </label>
+        </div>
+        <div class="form-check">
+          <label class="form-check-label">
+            <input class="form-check-input" type="radio" name="quality" value="ncook">
+            The wasn't cooked properly.
+          </label>
+        </div>
+        <div class="form-group">
+          <label for="textarea">Remarks: </label>
+          <textarea class="form-control" name="textfeed" id="textarea" rows="5" placeholder="Enter your remarks here!"></textarea>
+        </div>
+        <div class="row justify-content-center">
+          <input type="submit" name="submit" class="btn btn-outline-success" value="Submit Feedback!?">
+        </div>
+      </form>
     </div>
   </div>
+  <br>
+  <div class="container">
+    <div class="row justify-content-center">
+      <h3>Please enter the food requirements for the next day.</h3>
+    </div>
+  </div>
+  <div class="row justify-content-center">
+    <form action="requirement2.php" method="post">
+      <div class="row justify-content-center">
+        <p>Do you want food for tommorow?</p>
+      </div>
+      <div class="row justify-content-center">
+        <div class="form-check form-check-inline">
+          <label class="form-check-label">
+            <input class="form-check-input" type="radio" name="holiday" id="yesCheck" onclick="holidayCheck()" value="yes"> Yes
+          </label>
+        </div>
+        <div class="form-check form-check-inline">
+          <label class="form-check-label">
+            <input class="form-check-input" type="radio" name="holiday" id="noCheck" onclick="holidayCheck()" value="no"> No
+          </label>
+        </div>
+      </div>
+      <div class="row justify-content-center" id="ifNo" style="display: none;">
+        <div class="row form-group">
+          <textarea class="form-control" id="textArea" rows="5" placeholder="Please tell us the reason for your holiday!"></textarea>
+        </div>
+        <div class="row justify-content-center" id="date">
+          <div class="col-sm-4 col-md-4">
+            <select class="custom-select form-control" name="cYear">
+              <option value="0" style="display: none;" selected>Year</option>
+              <?php
+              date_default_timezone_set('Asia/Kolkata');
+              $today = getdate();
+              $year = $today[year];
+              $i = $year + 3;
+              $year = $year - 1;
+              while ($year < $i) {
+                ?>
+                <option value="<?php echo "$year"; ?>"><?php echo "$year"; ?></option>
+                <?php
+                $year = $year + 1;
+              }
+              ?>
+            </select>
+          </div>
+          <div class="col-sm-4 col-md-4">
+            <select class="custom-select form-control" name="cMonth">
+              <option value="0" style="display: none;" selected>Month</option>
+              <?php
+              $a = 1;
+              while ($a <= 12) {
+                ?>
+                <option value="<?php echo "$a"; ?>"><?php echo "$a"; ?></option>
+                <?php
+                $a = $a + 1;
+              }
+              ?>
+            </select>
+          </div>
+          <div class="col-sm-4 col-md-4">
+            <select class="custom-select form-control" name="cDate">
+              <option value="0" style="display: none;" selected>Date</option>
+              <?php
+              $a = 1;
+              while ($a <= 31) {
+                ?>
+                <option value="<?php echo "$a"; ?>"><?php echo "$a"; ?></option>
+                <?php
+                $a = $a + 1;
+              }
+              ?>
+            </select>
+          </div>
+          <br><br>
+        </div>
+      </div>
+      <br>
+      <div class="row justify-content-center">
+        <div class="col-sm-9 col-md-9 row justify-content-center">
+          <h4><?php echo "$school_name"; ?></h4>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <!-- <label class="col-form-label" for="select1">Rice</label>
+        <div class="col-sm-8 col-md-8">
+          <select class="custom-select form-control" name="selectRice" id="select1">
+            <option style="display: none;" selected value="">Open this select menu</option>
+            <option value="rice">Rice</option>
+            <option value="jRice">Jeera Rice</option>
+            <option value="khichdi">Khichdi</option>
+            <option value="mPulav">Mutter Pulav</option>
+            <option value="vPulav">Veg. Pulav</option>
+          </select>
+        </div> -->
+        <h5>Rice</h5>
+      </div>
+      <br>
+      <div class="row justify-content-center">
+        <!-- <label class="col-form-label" for="ris">Small</label> -->
+        <div class="col-md-3 col-sm-3">
+          <input type="text" style="border: none;" readonly name="ris" value="Small">
+        </div>
+        <div class="col-md-6 col-sm-6">
+          <input type="number" id="ris" name="qris" class="form-control" min="0" max="5" value="0" placeholder="Number of Vessels">
+        </div>
+      </div>
+      <br>
+      <div class="row justify-content-center">
+        <!-- <label class="col-form-label" for="rim">Medium</label> -->
+        <div class="col-md-3 col-sm-3">
+          <input type="text" style="border: none;" readonly name="rim" value="Medium">
+        </div>
+        <div class="col-md-6 col-sm-6">
+          <input type="number" id="rim" name="qrim" class="form-control" min="0" max="5" value="0" placeholder="Number of Vessels">
+        </div>
+      </div>
+      <br>
+      <div class="row justify-content-center">
+        <!-- <label class="col-form-label" for="ril">Large</label> -->
+        <div class="col-md-3 col-sm-3">
+          <input type="text" style="border: none;" readonly name="ril" value="Large">
+        </div>
+        <div class="col-md-6 col-sm-6">
+          <input type="number" id="ril" name="qril" class="form-control" min="0" max="5" value="0" placeholder="Number of Vessels">
+        </div>
+      </div>
+      <br>
+      <div class="row justify-content-center">
+        <!-- <label class="col-form-label" for="select2">Dal</label>
+        <div class="col-sm-8 col-md-8">
+          <select class="custom-select form-control" name="selectDal" id="select2">
+            <option style="display: none;" selected value="">Select Dal/Sabji</option>
+            <option value="dal">Dal</option>
+            <option value="sabji">Sabji</option>
+          </select>
+        </div> -->
+        <h5>Dal/Sabji</h5>
+      </div>
+      <br>
+      <div class="row justify-content-center">
+        <!-- <label class="col-form-lable" for="das">Small</label> -->
+        <div class="col-md-3 col-sm-3">
+          <input type="text" style="border: none;" readonly name="das" value="Small">
+        </div>
+        <div class="col-md-6 col-sm-6">
+          <input type="number" id="das" name="qdas" class="form-control" min="0" max="5" value="0" placeholder="Number of Vessels">
+        </div>
+      </div>
+      <br>
+      <div class="row justify-content-center">
+        <!-- <label class="col-form-lable" for="dam">Medium</label> -->
+        <div class="col-md-3 col-sm-3">
+          <input type="text" style="border: none;" readonly name="dam" value="Medium">
+        </div>
+        <div class="col-md-6 col-sm-6">
+          <input type="number" id="dam" name="qdam" class="form-control" min="0" max="5" value="0" placeholder="Number of Vessels">
+        </div>
+      </div>
+      <br>
+      <div class="row justify-content-center">
+        <!-- <label class="col-form-lable" for="dal">Large</label> -->
+        <div class="col-md-3 col-sm-3">
+          <input type="text" style="border: none;" readonly name="dal" value="Large">
+        </div>
+        <div class="col-md-6 col-sm-6">
+          <input type="number" id="dal" name="qdal" class="form-control" min="0" max="5" value="0" placeholder="Number of Vessels">
+        </div>
+      </div>
+      <br>
+      <div class="row justify-content-center">
+        <!-- <label class="col-form-label" for="select3">Roti</label>
+        <div class="col-sm-8 col-md-8">
+          <select class="custom-select form-control" name="selectRoti" id="select3">
+            <option style="display: none;" selected value="">Select Roti Type</option>
+            <option value="roti">Roti</option>
+            <option value="thepla">Thepla</option>
+          </select>
+        </div> -->
+        <h5>Roti</h5>
+      </div>
+      <br>
+      <div class="row justify-content-center">
+        <!-- <label class="col-form-lable" for="ros">Small</label> -->
+        <div class="col-md-3 col-sm-3">
+          <input type="text" style="border: none;" readonly name="ronum" value="Number">
+        </div>
+        <div class="col-md-6 col-sm-6">
+          <input type="number" id="rnum" name="rnum" class="form-control" min="0" max="5000" value="0" placeholder="Number of Rotis">
+        </div>
+      </div>
+      <br>
+      <input type="hidden" name="routeNo" value="<?php echo $routeNo; ?>" readonly disabled>
+
+      <!-- Block for custom date input, shifted on top -->
+
+      <div class="row justify-content-center">
+        <input class="btn btn-success col-sm-6 col-md-6 form-control" type="submit" name="submit" value="Send">
+      </div>
+      <br>
+    </form>
+
+  </div>
 </div>
+
+<script type="text/javascript">
+
+function holidayCheck() {
+    if (document.getElementById('noCheck').checked) {
+        document.getElementById('ifNo').style.display = 'block';
+    }
+    else document.getElementById('ifNo').style.display = 'none';
+}
+</script>
